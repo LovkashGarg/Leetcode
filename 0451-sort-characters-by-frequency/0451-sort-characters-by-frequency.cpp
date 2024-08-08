1ab85108391b77e9a46bf691e7e42848fde981f4
lovkash Garg <123955635+lovkashGarg555@users.noreply.github.com>
2024-08-08 18:32:49 +0530

    Time: 13 ms (37.68%), Space: 15.7 MB (5.06%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        vector<vector<char>>bucket(s.length()+1);
+      vector<int> frq(62,0);
+         int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]>=97 && s[i]<=122){
+                frq[s[i]-'a']++;
+            }
+            else if(s[i]>=65 && s[i]<=90){
+                frq[s[i]-'A'+ 26]++;
+            }
+            else if(s[i]>=48 && s[i]<=57){
+                frq[s[i]-'0'+ 52]++;
+            }
+        }
+        int maxi=0;
+        for(int i=0;i<62;i++){
+            // cout<<m[i+'a']
+            if(i<26){
+            bucket[frq[i]].push_back(i+'a');
+            }
+            else if(i>=26 && i<52){
+             bucket[frq[i]].push_back(i-26+ 'A');
+            }
+            else{
+            bucket[frq[i]].push_back(i-52+ '0');
+            }
+            maxi=max(maxi,frq[i]);
+        }
+        string ans;
+        int k=0;
+        for(int i=maxi;i>=1;i--){
+            if(k== n){
+                // cout<<"hello"<<endl;
+                break;
+            }
+            // cout<<"frequency "<<i<<endl;
+            for(auto j: bucket[i]){
+                string d=string(i,j);
+                ans+=d;
+                k+=i;
+            }
+        }
+        return ans;
+    }
+};
