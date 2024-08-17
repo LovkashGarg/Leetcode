ce1ee981710d95477e870b04aef08922eb539135
lovkash Garg <123955635+lovkashGarg555@users.noreply.github.com>
2024-08-18 01:19:56 +0530

    Time: 3 ms (26.21%), Space: 8.4 MB (29.78%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     int reverse(int x) {
-        string s=to_string(x);
-        if(s.length()<=1){
-          return x;
-        }
         bool flag=true;
-        // cout<<s<<endl;
-        
-        if(s[0]=='-'){
-         flag=false;
-        //  cout<<"negative"<<endl;
-         s.erase(0,1);
-        }
-         int n=s.length();
-        for(int i=0;i<n/2;i++){
-            swap(s[i],s[n-i-1]);
-        }
-        if(s[0]=='0'){
-            s.erase(0,1);
-        }
-        long long int  number=0;
-        for(int i=0;i<s.length();i++){
-            number=number*10+ int(s[i]-'0');
-            if(number>(INT_MAX)){
-                cout<<number<<endl;
+        if(x<0 ){
+            if(x==INT_MIN){
                 return 0;
             }
+            flag=false;
+            x=x*-1;
+        }
+        long long int number=0;
+        cout<<INT_MAX<<endl;
+        while(x!=0){
+            int d=x%10;
+            cout<<number<<endl;
+            number=number*(10) + d;
+            if(number>INT_MAX){
+
+                // cout<<"Hello "<<endl;
+                return 0;
+            }
+            x=x/10;
         }
         if(flag==false){
-            long long int d=-1;
-            number=number*d;
+            number=number*(-1);
         }
         return number;
     }
