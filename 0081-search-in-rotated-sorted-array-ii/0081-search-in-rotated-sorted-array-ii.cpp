598ecd5e900957498d0eac2edc602f25e3e06842
lovkash Garg <123955635+lovkashGarg555@users.noreply.github.com>
2024-08-23 18:21:59 +0530

    Time: 7 ms (31.7%), Space: 16.9 MB (21.96%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -21,17 +21,23 @@ public:
             }
           else if(nums[low]<=nums[mid] ){
               
-              if(nums[low]<=target && target<nums[mid] ){
+              if(nums[low]<target && target<nums[mid] ){
                     high=mid-1;
+              }
+              else if(nums[low]==target){
+                return true;
               }
               else{
                 low=mid+1;
               }
           }
           else {
-               if(nums[mid]<target && target<=nums[high]){
+               if(nums[mid]<target && target<nums[high]){
                  low=mid+1;
                }
+                else if(nums[high]==target){
+                return true;
+              }
                else{
                 high=mid-1;
                }
