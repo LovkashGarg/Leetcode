87500fd4d1856706578672c3a29af29f0b4fc221
lovkash Garg <123955635+lovkashGarg555@users.noreply.github.com>
2024-08-23 17:37:22 +0530

    Time: 2 ms (63.87%), Space: 14 MB (30.88%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low)+ (high-low)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            // identify the sorted half 
+          // left sorted 
+            else if(arr[low] <=arr[mid] ) {
+              // check if the target present in between 
+            
+                if(arr[low]<=target && target <arr[mid]){
+                     high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            //  right part is sorted 
+            else if(arr[high] >=arr[mid]  ){
+                  // check if the element is present or not
+                  if(arr[mid]<target&& target <= arr[high]){
+                   low=mid+1;
+                  }
+                  else{
+                    high=mid-1;
+                  }
+            }
+        }
+        return -1;
+
+    }
+};
