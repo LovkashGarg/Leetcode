fad3da9d163997c04826620f4b2c9a850fad17c5
lovkash Garg <123955635+lovkashGarg555@users.noreply.github.com>
2024-08-24 17:45:37 +0530

    Time: 92 ms (97.98%), Space: 69.2 MB (66.72%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,28 +2,22 @@ class Solution {
 public:
     int findBouqetsformed(vector<int>& bloomDay,int mid, int m, int k){
         int n=bloomDay.size();
-        vector<int> copiedbloomDay(bloomDay.begin(),bloomDay.end());
-        for(int i=0;i<n;i++){
-          if(bloomDay[i]<=mid){
-            copiedbloomDay[i]=0; // 0 means it is bloomed 
-          }
-        }
-
+      
         // check no of bouqets formed 
         int j=0;
         int bouqets=0;
-        cout<<"Starting "<<endl;
+        // cout<<"Starting "<<endl;
         while(j<n){
             // means 
-            if(copiedbloomDay[j]==0){
+            if(bloomDay[j]<=mid){
                int count=0;
-               while(j<n && copiedbloomDay[j]==0 && count<k){
+               while(j<n && bloomDay[j]<=mid && count<k){
                count++;
                j++;
                }
                if(count==k){
                 bouqets++;
-                cout<<"ending" <<j<<endl;
+                // cout<<"ending" <<j<<endl;
                 continue;
                }
             }
@@ -37,19 +31,21 @@ public:
             return -1; // not possible 
         }
         int maxi=INT_MIN;
+        int mini=INT_MAX;
 
         for(int i=0;i<n;i++){
             maxi=max(maxi,bloomDay[i]);
+            mini=min(mini,bloomDay[i]);
         }
         // maxi is the number of days max it could take 
-        int low=1;
+        int low=mini; // decrease the range to mini to maxi 
         int high=maxi;
         int ans=maxi;
         while(low<=high){
             int mid=(low)+ (high-low)/2;
-              cout<<mid<<endl;
+            //   cout<<mid<<endl;
             int noofbouqets=findBouqetsformed(bloomDay,mid,m,k);
-            cout<<noofbouqets << mid<<endl;
+            // cout<<noofbouqets << mid<<endl;
             if(noofbouqets<m){
               low=mid+1; // means we require more days 
             }
