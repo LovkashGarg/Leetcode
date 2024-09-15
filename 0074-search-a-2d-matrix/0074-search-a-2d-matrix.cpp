deaf5512fdbe34f5d9dcfbed48352d4e87df08cf
lovkash Garg <123955635+lovkashGarg555@users.noreply.github.com>
2024-09-15 18:00:02 +0530

    Time: 7 ms (21.94%), Space: 12.2 MB (42.93%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int lowrow = 0;
-        int highrow = matrix.size()-1;
-        while (lowrow <= highrow) {
-            int lowcol = 0;
-            int highcol = matrix[0].size()-1;
-            int midrow = (lowrow + highrow) / 2.0;
-            if (matrix[midrow][lowcol] <= target &&
-                target <= matrix[midrow][highcol]) {
-                while (lowcol <= highcol) {
-                    int midcol = (lowcol + highcol) / 2.0;
-                    if (matrix[midrow][midcol] == target) {
-                        return true;
-                    } else if (matrix[midrow][midcol] < target) {
-                        lowcol = midcol + 1;
-                    } else {
-                        highcol = midcol - 1;
-                    }
-                }
-                return false;
-            } else if (matrix[midrow][lowcol] > target) {
-                highrow = midrow - 1;
-            } else {
-                lowrow = midrow + 1;
+        int low=0;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int high=(n*m)-1;
+
+        while(low<=high){
+            int mid=(low+ high)/2;
+
+            int rowindex=mid/m; // converting to 2 D element point 
+            int colindex=mid%m;
+            if(matrix[rowindex][colindex]==target){
+                return true;
+            }
+            else if(matrix[rowindex][colindex]<target){
+               low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return false;
