f02e30b2fa57c38f6b7dbda2a058e66691456b40
lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-09-20 02:05:04 +0530

    Time: 204 ms (5.25%), Space: 69 MB (62.9%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<double> successProb_toPath(n,-1); // max probability is 1 
+       vector<pair<double,int>>adj[n];
+       successProb_toPath[start_node]=1;
+        for(int j=0;j< edges.size();j++){
+            adj[edges[j][0]].push_back({succProb[j],edges[j][1]});
+            adj[edges[j][1]].push_back({succProb[j],edges[j][0]});
+        }
+        priority_queue<pair<double,int>>pq;// where first is the probablility to the path
+        // and second is the node name 
+        pq.push({1,start_node});
+        while(!pq.empty()){
+            // I am trying to extract the node with max prob
+            double prob=pq.top().first;
+            int node=pq.top().second;
+          cout<<node <<" "<<prob<<endl;
+            pq.pop();
+            
+            for(auto j: adj[node]){
+              double prob_of_edg=j.first;
+              int adj_node=j.second;
+
+              double newprob=prob_of_edg*prob;
+              if(newprob >successProb_toPath[adj_node] ){
+                successProb_toPath[adj_node]=newprob;
+                pq.push({newprob,adj_node});
+              }
+            }
+        }
+        if(successProb_toPath[end_node]!=-1){
+        return successProb_toPath[end_node];
+        }
+        return double(0);
+    }
+};
