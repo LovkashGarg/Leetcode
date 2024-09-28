afc310fd9fa520bf70d0eb7ceaee1aab2b174894
lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-09-28 13:30:45 +0530

    Time: 0 ms (100%), Space: 15.3 MB (45.31%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   void solve(vector<string> &ans,int left, int right, int n, string s){
+      if(s.length()==2*n){
+        ans.push_back(s);
+        return;
+      }
+
+       if(left<n){
+        solve(ans, left+1, right,n, s+'(');
+       }
+       if(right <left){
+         solve(ans, left, right+1,n, s+')');
+       }
+
+   }
+    vector<string> generateParenthesis(int n) {
+         string s="";
+         vector<string> ans;
+         solve(ans,0,0,n,s);
+         return ans;
+    }
+};
