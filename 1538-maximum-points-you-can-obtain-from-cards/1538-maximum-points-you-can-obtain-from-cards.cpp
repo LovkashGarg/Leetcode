2d9e4bcf9347c1a996ee1172640b823a72cffdfd
lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-09-29 11:35:24 +0530

    Time: 37 ms (80.94%), Space: 45.1 MB (8.01%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        int n=cardPoints.size();
-      
-        int totalsum=accumulate(cardPoints.begin(),cardPoints.end(),0);
-        if(k==n){
-            return totalsum;
-        }
-        int d=0; // now we are trying to remove a sub array with n-k that have min sum 
-        int l=0;
-        int r=(n-k-1);
-        int sumcurr=0;
-        for(int i=0;i<r;i++){
-        sumcurr+=cardPoints[i];
-        }
-        // cout<<sumcurr<<endl;
-       long long int answer=0;
-        while(r<n){
-            sumcurr=sumcurr+ cardPoints[r];
-            r++;
-            if(answer< (totalsum- sumcurr)){
-                answer=totalsum-sumcurr;
-            }
-            sumcurr=sumcurr-cardPoints[l];
-            l++;
-        }
+     int lsum=0,rsum=0,maxsum=0;
+     // taken all the elements 
+     for(int i=0;i< k;i++){
+         lsum+= cardPoints[i];
+     }
+     int n=cardPoints.size();
+     int rindex=n-1;
+     maxsum =lsum;
+// means i am trying to remove element of left and add one element in right 
+     for(int j=k-1;j>=0;j--){
+      lsum =lsum -cardPoints[j];
 
-        return answer;
-          
+      rsum =rsum + cardPoints[rindex];
+      rindex--;
+      maxsum=max(maxsum ,lsum + rsum);
+     }
+     return maxsum;
     }
 };
