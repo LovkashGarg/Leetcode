8d4fbcf3eb3770ed9883f6b3c3a930f88c7f7623
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-10-16 00:18:09 +0530

    Time: 56 ms (50.29%), Space: 173.9 MB (21.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
+    string expandfromcenter(string s, int left,int right){
+    while(left>=0 && right<s.length() && s[left]==s[right]){
+        left--;
+        right++;
+    }
+     return s.substr(left+1, right-left-1);
+    }
     string longestPalindrome(string s) {
         string res;
-        if (s.length() == 1) {
+        string maxs;
+        if(s.size()<=1){
             return s;
         }
-        res+=s[0];
-        for (int i = 0; i < s.length(); i++) {
-            int l = i - 1, r = i + 1;
-            string ans;
-            ans += s[i];
-            // res=ans;
-            // expanding from center
-            while (l >= 0 && r < s.length() && s[l] == s[r]) {
-                ans = s[l] + ans + s[r];
-                l--;
-                r++;
-                if (ans.length() >= res.length()) {
-                    res = ans;
-                }
-            }
+        maxs+=s[0];
+        for(int i=0;i<s.length()-1;i++){
+            string odd=expandfromcenter(s,i,i);
+            string even=expandfromcenter(s,i,i+1);
 
-            l = i;
-            r = i + 1;
-            ans = "";
-            while (l >= 0 && r < s.length() && s[l] == s[r]) {
-                ans = s[l] + ans + s[r];
-                l--;
-                r++;
-                if (ans.length() >= res.length()) {
-                    res = ans;
-                }
+            if(maxs.length() <odd.length()){
+                maxs=odd;
             }
-            cout<<res<<endl;
+            if(maxs.length()<even.length()){
+                maxs=even;
+            }
         }
-        return res;
+        return maxs;
     }
 };
