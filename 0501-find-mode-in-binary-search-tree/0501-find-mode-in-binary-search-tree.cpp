68d9bb7a2432dbc5c3dd7c51bc6e75f15ab532b0
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-10-16 12:00:21 +0530

    Time: 18 ms (36.36%), Space: 22.5 MB (93.62%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,6 +11,8 @@
  */
 class Solution {
 public:
+    int currfreq=0, maxfreq=0, preele=INT_MIN;
+    vector<int>res;
     void dfs(TreeNode *root, unordered_map<int,int>&m,int &maxfrq){
         if(root==nullptr){
             return;
@@ -22,17 +24,32 @@ public:
         maxfrq=max(maxfrq,m[root->val]);
         dfs(root->right,m,maxfrq);
     }
+    void inorder(TreeNode *root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    if(preele==root->val){
+        currfreq++;
+    }
+    else{
+        currfreq=1;
+    }
+
+    if(currfreq >maxfreq){
+        res.clear(); // to clear all the element 
+        res.push_back(root->val);
+        maxfreq=currfreq;
+    }
+    else if(currfreq==maxfreq){
+        res.push_back(root->val);
+    }
+    preele=root->val;
+    inorder(root->right);
+    }
     vector<int> findMode(TreeNode* root) {
        // inorder travesal
-       unordered_map<int,int>m;
-       int maxfrq;
-       dfs(root,m,maxfrq); 
-       vector<int>ans;
-       for(auto i:m){
-        if(i.second==maxfrq){
-           ans.push_back(i.first);
-        }
-       }
-       return ans;
+      inorder(root);
+       return res;
     }
 };
