366a7630af5eb8afe3658cab73edae4c89dfbb11
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-11-05 00:06:00 +0530

    Time: 2 ms (78.4%), Space: 7.5 MB (31.59%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+    vector<int> m;  // Declare m without initializing here
+
+    Solution() : m(100, -1) {}  // Initialize m in the constructor
+
     int fib(int n) {
-        if(n<=1){
+        if (n <= 1) {
             return n;
         }
-        return fib(n-1)+ fib(n-2);
+        if (m[n] != -1) {
+            return m[n];
+        }
+        
+        m[n] = fib(n - 1) + fib(n - 2);
+        return m[n];
     }
 };
