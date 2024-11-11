da84bde91a70a0f8f9319da0f7081343881b2c86
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-11-11 10:36:08 +0530

    Time: 4 ms (55.96%), Space: 28.3 MB (66.45%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,55 +1,46 @@
 class Solution {
 public:
     bool primeSubOperation(vector<int>& nums) {
-
         int n = nums.size();
         int maxi = 1;
         for (int i = 0; i < n; i++) {
             maxi = max(maxi, nums[i]);
         }
 
+        // Generate primes up to maxi
         vector<bool> prime(maxi + 1, true);
-
         for (int p = 2; p * p <= maxi; p++) {
-            if (prime[p] == true) {
-                // Update all multiples of p greater than or equal
-                for (int i = p * p; i <= maxi; i += p)
+            if (prime[p]) {
+                for (int i = p * p; i <= maxi; i += p) {
                     prime[i] = false;
-            }
-        }
-       
-
-    //    for(int i=2;i<=maxi;i++){
-    //     cout<<i<<" "<<prime[i]<<endl;
-    //    }
-    //    cout<<endl;
-
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i + 1] > nums[i]) {
-                continue;
-            } else {
-                // I have to subtract it with the least posible
-                // which makes it greater then
-                // choose one prime number if it makes our number just less than
-                // the next number
-
-                for (int j = 2; j < nums[i]; j++) {
-                    if (prime[j ] && nums[i] - j < nums[i + 1]) {
-                        // cout << "PRIME " << j << " " << i << endl;
-                        nums[i] = nums[i] - j;
-                        break;
-                    }
                 }
             }
         }
 
-      bool flag=true;
-       for(int i=0;i<n-1;i++){
-        if(nums[i] >=nums[i+1]){
-         flag=false;
+        vector<int> primes;
+        for (int i = 2; i <= maxi; i++) {
+            if (prime[i]) {
+                primes.push_back(i);
+            }
         }
-       }
-        return flag;
+
+        // Traverse from the end to the start to ensure strictly increasing order
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (nums[i] >= nums[i + 1]) {
+                // if agar same hi rahega to nikal hi do 
+                bool found = false;
+                for (int j = 0; j < primes.size() && primes[j] < nums[i]; j++) {
+                    if (nums[i] - primes[j] < nums[i + 1]) {
+                        nums[i] -= primes[j];
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) return false; // If no valid prime subtraction found
+            }
+        }
+
+        return true;
     }
 };
