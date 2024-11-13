96fb97545c5b58c5a4b647fd84d8fc83a6a2c464
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-11-13 17:03:03 +0530

    Time: 25 ms (30.87%), Space: 26.4 MB (21.55%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        // Here I can make a 2d array which contains freq of all the 26 elements 
+        unordered_map<string,vector<string>>m;
+         for(auto i:strs){
+            string word=i;
+            sort(word.begin(),word.end());
+            m[word].push_back(i);
+         }
+         vector<vector<string>>ans;
+         for(auto i:m){
+vector<string>temp;
+            for(auto j: i.second){
+                temp.push_back(j);
+             
+            }
+            ans.push_back(temp);
+          
+         }
+        return ans;
+    }
+};
