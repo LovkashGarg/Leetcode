45e2893ce6076ea19280023d6a8bbfa08895783d
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-12-05 13:04:45 +0530

    Time: 4 ms (90.23%), Space: 22 MB (33.33%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -1,57 +1,54 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
+        int a = start.size() - 1;
+        int b = target.size() - 1;
+        while (a >= 0 && b >= 0) {
 
-     priority_queue<pair<int,char>>pq1;
-     priority_queue<pair<int,char>>pq2;
-     for(int i=0;i<start.size();i++){
-     if(start[i]=='L' || start[i]=='R'){
-       pq1.push({i,start[i]});
-     }
-     }
+            while (a >= 0 && start[a] == '_') {
+                a--;
+            }
+            while (b >= 0 && target[b] == '_') {
+                b--;
+            }
 
-     for(int i=0;i<target.size();i++){
-    if(target[i]=='L' || target[i]=='R'){
-        pq2.push({i,target[i]});
-    }
-     }
+            if (a >= 0 && b >= 0) {
+                if (start[a] != target[b]) {
+                    return false;
+                }
+                else if(a==b ){
+                    a--;
+                    b--;
+                }
+                else if (a < b && start[a] == 'R') {
+                    a--;
+                    b--;
+                } else if (a > b && start[a] == 'L') {
+                    a--;
+                    b--;
+                } else {
+                    return false;
+                }
+            }
+            // koi extra element hai 
+            else if( a< 0 && b>=0){
+                return false;
+            }
+            else  if(a>=0 && b<0){
+                return false;
+            }
+        }
 
-     // now my task is to find the Index which can satisfy his need 
-   if(pq1.size() !=pq2.size()){
-    return false;
-   }
-   while(!pq1.empty() && !pq2.empty()){
-    auto index1=pq1.top().first;
-    auto d1=pq1.top().second;
-    pq1.pop();
-
-    auto index2=pq2.top().first;
-    auto d2=pq2.top().second;
-    pq2.pop();
-
-    // since we cant jump over anyone relative order remain same 
-    if(d1 !=d2){
-        return false;
-    }
-
-    // matlab ki shift ho sakti hai 
-    if(index1==index2){
-        continue;
-    }
-   else if(index1 <index2 && d1 =='R'){
-   continue;
-   }
-   else if( index1 >index2 && d1=='L'){
-    continue;
-   }else{
-    return false;
-   }
-    // cout<<index1 << " "<<index2 << " "<< d1 << " "<<d2<<endl;
-   }
-  
-
-     return true;
-
-
+        while(a>=0 && start[a]=='_'){
+            a--;
+        }
+          while(b>=0 && target[b]=='_'){
+            b--;
+        }
+        if(a!=b){
+            return false;
+        }
+      
+        return true;
     }
 };
