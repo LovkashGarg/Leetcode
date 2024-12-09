3c03b9ce04e964575d6ccdf01776fd8101e04e19
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-12-09 22:10:40 +0530

    Time: 0 ms (100%), Space: 24 MB (10.7%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,67 +1,19 @@
 class NumArray {
 public:
-    vector<int>seq;
-    int d;
-    void construct (int index,int low ,int  high,vector<int>&nums){
-        // means there is only one element 
-
-        if(low==high){
-            seq[index]=nums[low];
-          return ;
-        }
-
-       int mid=(low+ high)/2;
-
-       construct(2*index+1, low,mid,nums);
-       construct(2*index+2,mid+1,high,nums);
-
-       seq[index]=seq[2*index+1]+seq[2*index+2];
-    }
+    vector<int>prefix;
+    vector<int>arr;
     NumArray(vector<int>& nums) {
         int n=nums.size();
-        seq=vector<int>(4*n);
-        int index=0;
-        int low=0;
-        int high=n-1;
-        d=n;
-        construct(index,low,high,nums);
-
-        for(int i=0;i<seq.size();i++){
-           cout<<seq[i]<<" ";
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+             prefix.push_back(sum);
+             arr.push_back(nums[i]);
         }
-        cout<<endl;
     }
-    int query(int index , int low,int high,int left,int right){
-
-
-     // No overlap 
-     /// low high l r or l r low high
-     if(high < left || right <low){
-        return 0;
-     }
-
-     // complete overlap 
-     //left low high right
- 
- // iska to sum return kar hi do 
-     if(low >=left && high<=right ){
-        return seq[index];
-     }
-
-     // Partial overlap 
-     int mid= (low+ high)/2;
-     int l= query(2*index+1, low,mid,left,right);
-     int r=query(2*index+2,mid+1, high, left,right);
-     return l+ r;
-     
-    }
+    
     int sumRange(int left, int right) {
-        // Now for a query I have 3 cases
-        // when no overlap 
-        int low=0;
-        int high=d-1;
-        return query(0,low,high,left,right);
-
+        return prefix[right]-prefix[left]+ arr[left];
     }
 };
 
