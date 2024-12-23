57596977399552ad6e67705b8c72402da485ccd5
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2024-12-23 14:17:23 +0530

    Time: 6 ms (6.99%), Space: 15.9 MB (5.19%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -10,32 +10,30 @@ return 1;
         return dp[index];
        }
     
-    int a=0,b=0,c=0,d=0,e=0;
+    int ans=0;
         //there can be two ways
          if(s[index]=='1'){
             // two possiblity 
-            a=solve(index+1,s,n,dp);
+            ans+=solve(index+1,s,n,dp);
             // and it can be
             if(index+1 <n ){
-            b=solve(index+2,s,n,dp);
+            ans+=solve(index+2,s,n,dp);
             }
         }
         else if(s[index]=='2'){
           
           // two possiblity 'B or 'VWXYZ'
-          c=solve(index+1,s,n,dp);
+          ans+=solve(index+1,s,n,dp);
           if(index+1<n && s[index+1]<='6'){
-          d= solve(index+2,s,n,dp);
+          ans+= solve(index+2,s,n,dp);
           }
         }
         else if(s[index]>='3' && s[index]<='9'){
-       e= solve(index+1,s,n,dp);
+       ans+= solve(index+1,s,n,dp);
         }
-        else{
-            return 0;
-        }
+       
 
-        dp[index]=a+b+c+d+e;
+        dp[index]=ans;
         return dp[index];
     }
     int numDecodings(string s) {
