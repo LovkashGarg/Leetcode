93c60a4a6b3a3bc63c42890964d28b8db5d408eb
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-01-03 23:22:52 +0530

    Time: 0 ms (100%), Space: 12.3 MB (5.42%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         
-        vector<vector<int>>a(1001);
         map<int,int>m;
         int n=arr.size();
+        int maxi=0;
         for(int i=0;i<n;i++){
             m[arr[i]]++;
+            maxi=max(maxi,m[arr[i]]);
         }
-
+      vector<int>visited(maxi+1,0);
        for(auto i:m){
-        cout<<i.first<<i.second<<endl;
-        if(a[i.second].size()==1){
+        if(visited[i.second]==1){
             // not unique
             return false;
         }
         else{
-            a[i.second].push_back(i.first);
+           visited[i.second]=1;
         }
        }
        return true;
