6d972ddc649a9536a0368f65a73f909bfc37bde3
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-01-03 22:58:33 +0530

    Time: 0 ms (100%), Space: 23.8 MB (51.24%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int l=0;
+        int r=0;
+        while(r<n){
+
+            if(nums[r]!=0){
+                swap(nums[r],nums[l]);
+                l++;
+            }
+             r++;
+        }
+
+    }
+};
