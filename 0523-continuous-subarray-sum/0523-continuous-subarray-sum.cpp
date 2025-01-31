9f4d9e0d2941347c2eb58536a77103deb2a2393b
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-01-31 13:43:11 +0530

    Time: 198 ms (5.05%), Space: 151.7 MB (7.49%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        long long prefixsum=0;
+        map<long long ,int>m;
+        int n=nums.size();
+        int maxlen=0;
+        m[0]=-1;
+        for(int i=0;i<n;i++){
+
+           prefixsum+=nums[i];
+            prefixsum%=k;
+
+        //    if(prefixsum==0){
+        //      maxlen=i+1;
+        //    }
+
+           if(m.find(prefixsum) !=m.end()){
+              maxlen=max(maxlen,i-m[prefixsum]);  
+           }
+           else{
+           m[prefixsum]=i;
+           }
+
+          
+        }
+
+          if(maxlen>=2){
+            return true;
+           }
+
+        return false;
+    }
+};
