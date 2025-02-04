fbdb8b7dd64d876c8e9fac177e4f2c87a0e1d06a
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-02-04 18:42:55 +0530

    Time: 20 ms (70.26%), Space: 17.7 MB (25.14%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,55 +1,53 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
-        sort(nums.begin(),nums.end());
 
-        // I want to find all the four length subarray 
+        sort(nums.begin(), nums.end());
+
+        // I want to find all the four length subarray
         set<vector<int>> ans;
-        int n=nums.size();
-        for(int i=0;i<n-3;i++){
-            for(int j=i+1;j<n-2;j++){
-                
-                 // I need to find the two element 
+        int n = nums.size();
+        for (int i = 0; i < n - 3; i++) {
+            for (int j = i + 1; j < n - 2; j++) {
 
-                 // my target to find the target - sum 
-                
-                 for(int k=j+1;k<n-1;k++){
-long long sum=nums[i]+ 0ll+ nums[j]+ nums[k];
- long long newtarget= target-sum;
-                  int left=k+1;
-                 int right=n-1;
-                 while(left<=right){
-                    int mid= (left+ right)/2;
-                    if(newtarget == nums[mid]){
-                        ans.insert({nums[i],nums[j],nums[k],nums[mid]});
-                        int val1= nums[left];
-                        int val2=nums[right];
-                       while(left< n && nums[left]== val1){
+                // I need to find the two element
+
+                // my target to find the target - sum
+
+                long long sum = nums[i] + 0ll + nums[j];
+                long long newtarget = target - sum;
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+
+                    if (newtarget == nums[left] + nums[right]) {
+                        ans.insert({nums[i], nums[j], nums[left], nums[right]});
+                        int val1 = nums[left];
+                        int val2 = nums[right];
+                        while (left < n && nums[left] == val1) {
+                            left++;
+                        }
+                        while (right >= 0 && nums[right] == val2) {
+                            right--;
+                        }
+
+                    } else if (newtarget > nums[left] + nums[right]) {
                         left++;
-                       }
-                       while(right>=0 && nums[right]== val2){
+                    } else {
                         right--;
-                       }
-
                     }
-                    else if(newtarget > nums[mid]){
-                        left=mid+1;
-                    }
-                    else{
-                        right=mid-1;
-                    }
-                 }
+                }
 
-                 }
-
-                
-
-
-
+                while (j + 1 < n && nums[j] == nums[j + 1]) {
+                    j++;
+                }
+            }
+            while (i + 1 < n && nums[i] == nums[i + 1]) {
+                i++;
             }
         }
 
-        return vector<vector<int>>(ans.begin(),ans.end());
-    }
-};
+    return vector<vector<int>>(ans.begin(), ans.end());
+}
+}
+;
