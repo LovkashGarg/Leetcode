8e72a664e047c91179329def5047d29eefe3941c
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-02-11 11:31:48 +0530

    Time: 3055 ms (5.02%), Space: 250.4 MB (60.18%) - LeetHub

---
+++ 3760-assign-elements-to-groups-with-constraints/3760-assign-elements-to-groups-with-constraints.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void solve(int divident, map<int, int>& m, int index,vector<int>&ans) {
+
+        // Now I can say
+        for (int j = 1; j * j <= divident; j++) {
+            // means perfect square
+            if ((divident / double(j)) == j && m.find(j)!=m.end()) {
+                ans[index]=min(m[j],ans[index]);
+            } else if (divident % j == 0) {
+               
+                if(m.find(j)!=m.end()){
+                     ans[index]=min(m[j],ans[index]);
+                }
+                if(m.find(divident/j)!=m.end()){
+ans[index]=min(m[divident/j],ans[index]);
+                }
+   
+            }
+
+        }
+    }
+    vector<int> assignElements(vector<int>& groups, vector<int>& elements) {
+        int n = elements.size();
+        int s = groups.size();
+        map<int, int> m;
+
+        // store the index of each divisor and it should be at min value 
+        for (int i = n-1; i >=0; i--) {
+            m[elements[i]] = i; 
+        }
+
+        // find all the factors of the each elemtn in groups
+        vector<int>ans(s,INT_MAX);
+        for (int i = 0; i < s; i++) {
+            cout<< " Hello"<<endl;
+            solve(groups[i], m,i, ans);
+        }
+        for(int i=0;i<s;i++){
+            if(ans[i]==INT_MAX){
+                ans[i]=-1;
+            }
+        }
+        return ans;
+
+        
+    }
+};
