74b9f853d257a7cd6f8f6227284335bb9f2a0883
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-02-12 18:48:57 +0530

    Time: 19 ms (78.72%), Space: 60.6 MB (91.74%) - LeetHub

--- 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.cpp
+++ 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+int rev(int n)   //function to find reverse of a number
+    {
+        int res=0;
+        while(n!=0)
+        {
+            res=res*10+n%10;
+            n/=10;
+        }
+        return res;
+    }
     int countNicePairs(vector<int>& nums) {
         
         int n= nums.size();
@@ -8,28 +18,13 @@ public:
         long count=0;
         
         for(int i=0;i<n;i++){
-            // find the reverse 
-            int newnum=nums[i];
-
-            if(nums[i] >=10){
-            string num=to_string(nums[i]);
-            reverse(num.begin(),num.end());
-            // remove leading zeroes
-            int j=0;
-            while(num[j] == 0 && j<num.size()){
-                j++;
-            }
-             num= num.substr(j);
-            newnum=stoi(num);
-            }
-            else{
-                newnum=nums[i];
-            }
-            
+            // find the reverse
+            int newnum= rev(nums[i]);      
             count=( count+ m[nums[i]- newnum])%mod;
             m[nums[i]-newnum]++;
 
         }
+
 
         return count;
 
