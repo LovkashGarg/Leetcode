0a4275ceb8d7b848cc8c43aac292577d9d2a4a32
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-02-12 19:42:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (27.2%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+
+       vector<int>first(26,-1); 
+       vector<int>last(26,-1); 
+
+       int n= s.size();
+       for(int i=0;i<n;i++){
+         if(first[s[i]-'a']==-1){
+          first[s[i]-'a']=i;
+         }
+
+         last[s[i]-'a']=i;
+       }
+
+       // now I have to merge this intervals 
+       vector<pair<int,int>>  intervals;
+       for(int i=0;i<26;i++ ){
+         if(first[i] != -1 && last[i]!=-1){
+            intervals.push_back({first[i],last[i]});
+         }
+       }
+
+       
+       sort(intervals.begin(),intervals.end());
+
+       vector<pair<int,int>> merged;
+       int start= intervals[0].first;
+       int end= intervals[0].second;
+
+       for(int i=1;i<intervals.size();i++){
+         if(end > intervals[i].first ){
+            end=max(end,intervals[i].second);
+         }
+         else{
+            merged.push_back({start,end});
+            start= intervals[i].first;
+            end=intervals[i].second;
+         }
+
+       }
+       merged.push_back({start,end});
+       vector<int>ans;
+       for(auto i:merged){
+        ans.push_back( i.second-i.first +1);
+       }
+       return ans;
+    }
+};
