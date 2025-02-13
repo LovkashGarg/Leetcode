d6a5e9ab8c8d1f4c6f5317afe18f6e103f5774d8
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-02-14 01:41:59 +0530

    Time: 67 ms (18.22%), Space: 17.3 MB (42.67%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    set<string> news;
+
+    void permut(int index, int n, string &m) {
+        if (index >= n) {
+            news.insert(m);
+            return;
+        }
+
+        for (int i = index; i < n; i++) {
+            swap(m[i], m[index]);
+            permut(index + 1, n, m);
+            swap(m[i], m[index]); 
+        }
+    }
+
+    void solve(int index, int n, string &tiles, string &output, set<string> &s) {
+        if (index >= n) {
+            if (!output.empty()) 
+                s.insert(output);
+            return;
+        }
+
+        output.push_back(tiles[index]);
+        solve(index + 1, n, tiles, output, s);
+        output.pop_back();
+        solve(index + 1, n, tiles, output, s);
+    }
+
+    int numTilePossibilities(string tiles) {
+        set<string> s; // To store unique subsequences
+        string output;
+        int n = tiles.size();
+        
+        solve(0, n, tiles, output, s);
+
+        for (auto it : s) {
+            string temp = it;
+            permut(0, temp.size(), temp); 
+        }
+
+        return news.size(); 
+    }
+};
