69f0a684b478c8639fce76f99c8fa834c0e47b0b
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-02-14 01:06:44 +0530

    Time: 49 ms (21.13%), Space: 69.6 MB (24.84%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -3,20 +3,15 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
       int count=0;
       int n=nums.size();
-      map<int,int>m; 
       sort(nums.begin(),nums.end());
-      m[nums[0]]=1;
       for(int i=1;i<n;i++){
         // here if it already exist 
-        if(m.find(nums[i])!=m.end()){
+        if(nums[i] <= nums[i-1]){
          int last=nums[i];
-          m[nums[i-1]+1]=1;
           nums[i]=nums[i-1]+1;
          count+= (nums[i-1]+1- last);
         }
-        else{
-            m[nums[i]]=1;
-        }
+        
       } 
       return count;
     }
