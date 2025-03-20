a743479dc5b59661c1fc665b0db2647acc079801
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-03-21 00:30:19 +0530

    Time: 3 ms (32.8%), Space: 16.1 MB (7.04%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
 
+// Important Initialize the dp array with 1e9 not -1 since -1 is int the range of dp and not satisfy it and hence it will revaluate its value and hence will give tle
+
+int ans=INT_MAX;
    int solve(int row_index,int col_index, int n, vector<vector<int>>&dp ,vector<vector<int>>& matrix  ){
      
     if(row_index>=n){
         return 0;
     }
+    
 
      if(dp[row_index][col_index] !=1e9){
         return dp[row_index][col_index];
@@ -16,6 +20,7 @@ public:
     if(col_index-1>=0){
     left_bottom= matrix[row_index][col_index]+ solve(row_index+1,col_index-1,n,dp,matrix);
     }
+
 
     int right_bottom=INT_MAX;
     if(col_index+1<n){
@@ -32,7 +37,6 @@ public:
 
        vector<vector<int>>dp(n,vector<int>(n,1e9));
 
-int ans=INT_MAX;
        // Here starting point can be various
        for(int i=0;i<n;i++){
        ans=min(ans, solve(0,i,n,dp,matrix)); 
