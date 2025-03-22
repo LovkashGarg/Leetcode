bd511002b92b0ae2f6b65be9064ead997fbb789c
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-03-22 23:00:05 +0530

    Time: 18 ms (9.11%), Space: 28 MB (5.17%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int,int>m;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            m[nums[i]]=1;
+        }
+
+        for(int i=0;i<n;i++){
+            if(!m.count(i)){
+return i;
+            }
+        }
+        
+        return n;
+    }
+};
