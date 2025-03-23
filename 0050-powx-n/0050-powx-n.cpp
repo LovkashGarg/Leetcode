e771aa555c72850ee6cf9c6083eb2af8bbb28ed5
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-03-23 15:51:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,6 +2,33 @@ class Solution {
 public:
     double myPow(double x, int n) {
         
-        return pow(x,double(n));
+        if(n==0){
+            return 1;
+        }
+        else if(n==1){
+            return x;
+        }
+        else if(n>0){
+            // even 
+            double val= myPow(x,n/2);
+           if(n%2==0){
+             return val*val;
+           }
+           else{
+            return x*val*val;
+           }
+        }
+        // n is negative 
+        else {
+          double val= myPow(x, -(n/2));
+          if(n%2==0){
+            return double(1)/val* double(1)/val;
+          }
+          else{
+             return (double(1)/x) *(double(1)/val)*(double(1)/val);
+          }
+
+        }
+
     }
 };
