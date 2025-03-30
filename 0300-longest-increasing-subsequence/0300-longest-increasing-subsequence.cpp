49da613439f7a829f4ae2ae34beeb44713a66192
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-03-31 00:19:28 +0530

    Time: 152 ms (34.39%), Space: 14.5 MB (38.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-   int solve(int prev, int curr, int n, vector<int> & nums, vector<vector<int>>&dp){
-    if(curr>=n){
-        return 0;
-    }
-
-    if(dp[prev+1][curr]!=-1){
-        return dp[prev+1][curr];
-    }
-
-
-    // include or exclude 
-    int first=0;
-    int second=0;
-    if(prev==-1){
-    first = 1+ solve(curr,curr+1,n,nums,dp);
-    }
-    else{
-        if(nums[prev]<nums[curr]){
-            second = 1+ solve(curr, curr+1,n,nums,dp);
-        }
-    }
-
-    int third= solve(prev,curr+1,n, nums,dp);
-
-    return dp[prev+1][curr]= max(first, max(second,third));
-
-   }
     int lengthOfLIS(vector<int>& nums) {
-         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(-1,0, n, nums,dp);
+        
+        int n= nums.size();
+        // here base case 
+        // dp[prev][n]=0; // already covered 
+        vector<int>current(n+1,0);
+        vector<int>next(n+1,0);
+
+        for(int curr=n-1;curr>=0;curr--){
+             for(int prev=curr-1;prev>=-1;prev--){
+                
+                 int exclude= next[prev+1];
+                 int include=0;
+
+                 if(prev==-1 || nums[prev] < nums[curr]){
+
+                    include= 1 + next[curr+1];
+
+                 }
+
+                 current[prev+1]= max(include, exclude); 
+             }
+             next=current;
+        }
+
+        return next[0];
     }
 };
