2619a3820fc1e1b798e88c928c896964b425213d
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-03-30 18:55:37 +0530

    Time: 5 ms (20.2%), Space: 19.8 MB (16.5%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-     int solve(int index, int buy, vector<int>&prices, int n,vector<vector<int>>&dp){
 
-    if(index==n){
-        return 0;
+    int solve_tab( int n, vector<int>&prices){
+      
+      // for base case 
+      vector<vector<int>>dp(n+1, vector<int>(2,0));
+
+      // I have to shift the index 
+      // means for -1 index max value is 0
+      dp[n][0]=0;
+      dp[n][1]=0;
+
+
+      for(int i=n-1;i>=0;i--){
+        // buy value 
+        for(int j=0;j<=1;j++){
+
+           if(j==0){
+            dp[i][j]=max(dp[i][j], max(-prices[i] + dp[i+1][1-j], dp[i+1][j]));
+           }
+           else{
+            dp[i][j]=max(dp[i][j], max(prices[i] +  + dp[i+1][1-j] , dp[i+1][j]));
+           }
+
+        }
+       
+      }
+
+// max profit with buying allowed 
+      return dp[0][0];
+
     }
-     if(dp[index][buy]!=INT_MIN){
-        return dp[index][buy];
-     }
-
-     int maxprofit=0;
-
-     if(buy==0){
-        int profit1= -prices[index] + solve(index+1, 1,prices,n,dp);
-        int profit2= solve(index+1, 0, prices,n,dp);
-        maxprofit=max({profit1,profit2,maxprofit});
-     }
-     else{
-        int profit1= prices[index] + solve(index+1, 0, prices,n,dp);
-        int profit2= solve(index+1, 1, prices,n,dp);
-          maxprofit=max({profit1,profit2,maxprofit});
-     }
-     return dp[index][buy]=maxprofit;
-
-     }
-
 
     int maxProfit(vector<int>& prices) {
         
         // since we can hold one stock so therefore we store it in a variable whether we can buy or not
         int n=prices.size();
-        vector<vector<int>>dp(n+1, vector<int>(2,INT_MIN));
-        return solve(0,0,prices,n,dp);
+        return solve_tab(n,prices);
     }
 };
