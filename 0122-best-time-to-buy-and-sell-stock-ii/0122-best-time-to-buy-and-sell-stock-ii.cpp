6a8a43e6e10ecea7d89516a3127f721e0d5b7f9b
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-03-30 18:32:29 +0530

    Time: 216 ms (5.92%), Space: 27 MB (5.15%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int boughtfor=prices[0];
-        int maxsellfor=prices[0];
-        int profit=0;
-        for(int i=1;i< prices.size();i++){
-            if(boughtfor > prices[i]){
-               profit+= ( maxsellfor - boughtfor);
-               maxsellfor=prices[i];
-               boughtfor=prices[i];
-            }else if(maxsellfor > prices[i]){
-                 profit+= ( maxsellfor - boughtfor);
-                 boughtfor= prices[i];
-                 maxsellfor=prices[i];
-            }
-            maxsellfor= max(maxsellfor, prices[i]);
-        }
-         profit+= ( maxsellfor - boughtfor);
-         return profit;
+     int solve(int index, int buy, vector<int>&prices, int n,vector<vector<int>>&dp){
 
+    if(index==n){
+        return 0;
+    }
+     if(dp[index][buy]!=INT_MIN){
+        return dp[index][buy];
+     }
+
+     int maxprofit=0;
+
+     if(buy==0){
+        int profit1= -prices[index] + solve(index+1, 1,prices,n,dp);
+        int profit2= solve(index+1, 0, prices,n,dp);
+        maxprofit=max({profit1,profit2,maxprofit});
+     }
+     else{
+        int profit1= prices[index] + solve(index+1, 0, prices,n,dp);
+        int profit2= solve(index+1, 1, prices,n,dp);
+          maxprofit=max({profit1,profit2,maxprofit});
+     }
+     return dp[index][buy]=maxprofit;
+
+     }
+
+
+    int maxProfit(vector<int>& prices) {
+        
+        // since we can hold one stock so therefore we store it in a variable whether we can buy or not
+        int n=prices.size();
+        vector<vector<int>>dp(n+1, vector<int>(2,INT_MIN));
+        return solve(0,0,prices,n,dp);
     }
 };
