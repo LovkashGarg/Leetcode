2c00910c18d2d531af1ee71df0cee9244869800c
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-04-10 00:34:56 +0530

    Time: 6 ms (57.8%), Space: 12.4 MB (85.52%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-   int solve(int index , int k , vector<int>&nums , int n ,vector<int>&dp ){
 
-    if(index==n ){
-        return 0;
-    }
-    if(dp[index]!=-1){
-        return dp[index];
-    }
-
-    // so firstly 
-    // total element is less then k 
-    int maxsum=0;
-    int maxi=INT_MIN;
-    for(int i = index ;i<index + k && i<n;i++){
-         maxi=max(maxi , nums[i]);
-         maxsum=max(maxsum ,  maxi *(i-index+1) + solve(i+1,k,nums,n,dp));
-    }
-
-     return  dp[index]= maxsum;
-   }
     int maxSumAfterPartitioning(vector<int>& nums, int k) {
      
       int n= nums.size();
-      vector<int>dp(n+1,-1);
-     // I need firstly the maxvalue , k check if number of element is less , and total sum 
-       return solve(0,k,nums,n,dp);
+      vector<int>dp(n+1,0);
+      dp[n]=0;
+      int maxsum =0;
+      for(int i=n-1;i>=0;i--){
+        int maxi=INT_MIN;
+           for(int j=i;j<min(i+k,n) ;j++){
+            maxi= max(maxi , nums[j]);
+            maxsum = max(maxsum , maxi *(j-i+1) +  dp[j+1]);
+           }
+        dp[i]= maxsum;
+
+      }
+
+    return dp[0];
+
     }
 };
