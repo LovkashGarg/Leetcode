608e99bd91f574c52edd7077724bd0b2aa1574e1
Lovkash Garg <123955635+LovkashGarg@users.noreply.github.com>
2025-04-25 21:50:30 +0530

    Time: 115 ms (6.01%), Space: 124 MB (30.6%) - LeetHub

---
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+
+        long long count=0;
+        long long equals=0;
+        map<int,int>m;
+
+        m[0]=1;
+
+        for(auto i:nums){
+
+            // here we update equal when we got a element with the condition 
+            if(i%modulo==k) equals++;
+
+            int rem=equals%modulo;
+            int needed= (rem-k + modulo) %modulo;
+            count+= m[needed]; // zChecking how much element are available with this value before
+            m[rem]++;
+        }  
+
+         return count;
+    }
+};
